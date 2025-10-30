aff7b1b10307d176999ce6c00f57e144691e2be3
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2025-10-30 10:41:08 -0400

    multiplication table done

--- Midterm_Brian_Pagsolingan/app/build.gradle.kts
+++ Midterm_Brian_Pagsolingan/app/build.gradle.kts
@@ -40,4 +40,22 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Room components
+    implementation("androidx.room:room-runtime-android:2.8.2")
+    annotationProcessor("androidx.room:room-compiler:2.8.2")
+
+    // Lifecycle componenents
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.9.4")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata:2.9.4")
+    // LifeCycles only without viewmodel or livedata
+    implementation("androidx.lifecycle:lifecycle-runtime:2.9.4")
+
+    // Saved state module for ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.9.4")
+
+    // Annotation processor
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.9.4")
 }
--- Midterm_Brian_Pagsolingan/app/src/main/java/com/example/midterm_brian_pagsolingan/MainActivity.java
+++ Midterm_Brian_Pagsolingan/app/src/main/java/com/example/midterm_brian_pagsolingan/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.midterm_brian_pagsolingan;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +28,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    EditText intinput = findViewById(R.id.etInput);
+    Button btnSubmit = findViewById(R.id.btnSubmit);
+    ListView multTable = findViewById(R.id.lvItems);
+
+    btnSubmit.setOnClickListener(v ->{
+        String inpuText = intinput.getText().toString().trim();
+
+        if(inpuText.isEmpty()){
+            Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        int number;
+        try{
+            number = Integer.parseInt(inpuText);
+        }catch (NumberFormatException e){
+            Toast.makeText(this, "Invalid number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        List<Integer> tableList = new ArrayList<>();
+        for(int i = 0; i <= 10; i++){
+            tableList.add(number * i);
+        }
+
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, tableList);
+        multTable.setAdapter(adapter);
+    });
+
     }
 }
